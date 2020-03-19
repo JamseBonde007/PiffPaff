85d98a528c7d0f27a6430db63d70ed92d8d6ca80
pilatmartin <8pilatmartin8@gmail.com>
2020-03-19 14:04:11 +0100

    timer a enemy bez podmienky

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,9 @@
 package sample;
 
+import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -18,7 +20,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Controller {
-    private Timer timer;
     @FXML
     private Button player1Btn, player2Btn, newGameBtn;
     @FXML
@@ -33,79 +34,94 @@ public class Controller {
     private int interval;
     private int level;
     private float time;
-    private float ciklus;
+    private float cyklus;
     private float setinterval;
-    private boolean run =false;
+    private boolean run = false;
+    private Timeline timeline;
+    private Timer timerGame = new Timer(), timerEnemy = new Timer();
 
 
     @FXML
-    public void countdown(){
-        run=true;
-        time=0;
-        ciklus=0;
+    public void countdown() {
+
+
+        time = 0;
+        cyklus = 0;
+
         player1Score.setText("0");
         player2Score.setText("0");
-        level=(int)difficultySlider.getValue();
-        int r=53-3*level;
+
+        level = (int) difficultySlider.getValue();
+        int r = 53 - 3 * level;
         blue.setRadius(r);
         red.setRadius(r);
         red.setLayoutX(413);
         red.setLayoutY(142);
-        setinterval=(float)r;
-        level=(int)((setinterval/35)*10);
 
-        interval=6*r;
+        setinterval = (float) r;
+        level = (int) ((setinterval / 35) * 10);
 
+        interval = 6 * r;
 
         countdownLabel.setText("" + interval / 60 + ":" + interval % 60);
-        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.1), event -> {
-            ciklus=(float) (ciklus+0.1);
-            time= (float) (time+0.1);
-            System.out.println((int)(ciklus*10));
-            if((int)(ciklus*10)==level){
-                visibleRed();
-                ciklus=0;
-            }
-            System.out.println(time);
-            if((int)(time*10)==10){
-                interval--;
-                countdownLabel.setText("" + interval / 60 + ":" + interval % 60);
-                System.out.println("idem");
-                System.out.println("" + interval / 60 + ":" + interval % 60);
-                time=0;
-            }
-        }));
-        timeline.setCycleCount(interval*10);
-        timeline.play();
-        if(interval==0)
-        run = false;
+
+        System.out.println(difficultySlider.getValue());
+        if (!run) {
+            timerGame.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    Platform.runLater(() -> {
+                        enemy(blue.getLayoutX(),blue.getLayoutY());
+                        run = true;
+                        cyklus = (float) (cyklus + 0.1);
+                        time = (float) (time + 0.1);
+                        //System.out.println((int) (cyklus * 10));
+
+                        if ((int) (cyklus * 10) == level) {
+                            visibleRed();
+                            cyklus = 0;
+                        }
+
+                        //System.out.println(time);
+
+                        if ((int) (time * 10) == 10) {
+                            interval--;
+                            String seconds;
+                            if (interval % 60<10){
+                                seconds = "0"+interval % 60;
+                            }else seconds = ""+interval % 60;
+                            countdownLabel.setText("" + interval / 60 + ":" + seconds);
+                            time = 0;
+                        }
+                    });
+                }
+
+            }, 0, 100);
+        }
+
+        if (interval == 0)
+            run = false;
 
     }
 
-
-
-
     private void visibleRed() {
-
         double randomX = Math.random() * (1000 - 2 * red.getRadius()) + red.getRadius();
         double randomY = Math.random() * (485 - 2 * red.getRadius()) + red.getRadius();
+
         red.setLayoutX(randomX);
         red.setLayoutY(randomY);
         red.setVisible(true);
-
-
     }
 
-
-
-    class Delta { double x, y; }
+    class Delta {
+        double x, y;
+    }
 
     @FXML
     public void moveMe() {
-        if(run==true) {
+        if (run) {
             final Delta dragDelta = new Delta();
             blue.setOnMousePressed(new EventHandler<MouseEvent>() {
-
 
                 public void handle(MouseEvent mouseEvent) {
                     // record a delta distance for the drag and drop operation.
@@ -115,7 +131,6 @@ public class Controller {
                 }
             });
             blue.setOnMouseReleased(new EventHandler<MouseEvent>() {
-
 
                 public void handle(MouseEvent mouseEvent) {
                     blue.setCursor(Cursor.HAND);
@@ -133,7 +148,6 @@ public class Controller {
             });
             blue.setOnMouseEntered(new EventHandler<MouseEvent>() {
 
-
                 public void handle(MouseEvent mouseEvent) {
                     blue.setCursor(Cursor.HAND);
                 }
@@ -142,8 +156,8 @@ public class Controller {
     }
 
     @FXML
-    public void clickRed(){
-        if (run==true) {
+    public void clickRed() {
+        if (run) {
             int score = Integer.parseInt(player1Score.getText());
             score++;
             player1Score.setText(String.valueOf(score));
@@ -151,13 +165,32 @@ public class Controller {
     }
 
     @FXML
-    public void clickBlue(){
-
+    public void clickBlue() {
+        if (run) {
+            int score = Integer.parseInt(player2Score.getText());
+            score++;
+            player2Score.setText(String.valueOf(score));
         }
+    }
 
+    public void enemy(double x, double y) {
 
+        if (!run) {
+            timerEnemy.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    Platform.runLater(() -> {
+                        System.out.println("x="+x+" blueX="+blue.getLayoutX());
+                        if ((blue.getLayoutX() >= x-blue.getRadius() && blue.getLayoutX() <= x+blue.getRadius()) || (blue.getLayoutY() >= x-blue.getRadius() && blue.getLayoutY() <= x+blue.getRadius())){
+                            clickRed();
+                            System.out.println("klik");
+                        }
 
-
+                    });
+                }
+            }, 1000, 150 * level);
+        }
+    }
 
 
 
